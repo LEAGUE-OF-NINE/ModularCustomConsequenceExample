0a1d597f977ce277a70383a0217f535522dc84fd
Not-Nova-Shane <ezpz12453@gmail.com>
2025-07-07 12:56:02 -0400

    Update LetheMain.cs
    spacing

--- Lethe/LetheMain.cs
+++ Lethe/LetheMain.cs
@@ -21,7 +21,7 @@ public class Main : BasePlugin
     {
         public void ExecuteConsequence(ModularSA modular, string section, string circledSection, string[] circles) // put all the actual consequence code inside this execute
         {
-        // The following code is taken from the "Break" consequence of Modular, staggering a unit.
+            // The following code is taken from the "Break" consequence of Modular, staggering a unit.
         
    	    var modelList = modular.GetTargetModelList(circles[0]); // gets target from first param 
 		if (modelList.Count < 1) return; 
@@ -33,11 +33,11 @@ public class Main : BasePlugin
 
 		foreach (BattleUnitModel targetModel in modelList) // loop over every possible target in the list
 		{
-            // code to change the ability source depending on what the script is run on, a Skill or a Passive
+            		// code to change the ability source depending on what the script is run on, a Skill or a Passive
 			ABILITY_SOURCE_TYPE abilitySourceType = ABILITY_SOURCE_TYPE.SKILL;
 			if (modular.abilityMode == 2) abilitySourceType = ABILITY_SOURCE_TYPE.PASSIVE;
 
-            // possible cases based off of previous parameters
+            		// possible cases based off of previous parameters
 			if (force) targetModel.BreakForcely(modular.modsa_unitModel, abilitySourceType, modular.battleTiming, false, modular.modsa_selfAction);
 			if (!force || both) targetModel.Break(modular.modsa_unitModel, modular.battleTiming, modular.modsa_selfAction);
 			if (resistancebreak) targetModel.ChangeResistOnBreak();
