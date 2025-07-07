775c60e4c67ba8ade4a53b3149dccbb6492882d1
pufferffish <me@windtfw.com>
2025-07-07 19:32:08 +0100

    reformat

--- Lethe/LetheMain.cs
+++ Lethe/LetheMain.cs
@@ -8,7 +8,8 @@ using ModularSkillScripts.Consequence;
 namespace ModularCustomConsequences;
 
 [BepInPlugin(GUID, NAME, VERSION)]
-[BepInDependency("GlitchGames.ModularSkillScripts")] // this is required to make your plugin run AFTER Modular has been loaded.
+// this is required to make your plugin run AFTER Modular has been loaded.
+[BepInDependency("GlitchGames.ModularSkillScripts")]
 public class Main : BasePlugin
 {
     // Edit the below to your own plugin name, version, etc.
@@ -17,39 +18,45 @@ public class Main : BasePlugin
     public const string AUTHOR = "LeagueOfNine";
     public const string GUID = $"{AUTHOR}.{NAME}";
 
-    public class myConsequenceClass : IModularConsequence // creates a class to be added to the consequence dict of modular 
+    // creates a class to be added to the consequence dict of modular 
+    public class MyConsequenceClass : IModularConsequence
     {
-        public void ExecuteConsequence(ModularSA modular, string section, string circledSection, string[] circles) // put all the actual consequence code inside this execute
+        // put all the actual consequence code inside this execute
+        public void ExecuteConsequence(ModularSA modular, string section, string circledSection, string[] circles)
         {
             // The following code is taken from the "Break" consequence of Modular, staggering a unit.
-        
-   	    var modelList = modular.GetTargetModelList(circles[0]); // gets target from first param 
-		if (modelList.Count < 1) return; 
-  
-		string opt2_string = circles.Length >= 2 ? circles[1] : "natural"; // if there is no 2nd argument in the param, use "natural"
-		bool force = opt2_string != "natural";
-		bool both = opt2_string == "both";
-		bool resistancebreak = circles.Length <= 2;
 
-		foreach (BattleUnitModel targetModel in modelList) // loop over every possible target in the list
-		{
-            		// code to change the ability source depending on what the script is run on, a Skill or a Passive
-			ABILITY_SOURCE_TYPE abilitySourceType = ABILITY_SOURCE_TYPE.SKILL;
-			if (modular.abilityMode == 2) abilitySourceType = ABILITY_SOURCE_TYPE.PASSIVE;
+            var modelList = modular.GetTargetModelList(circles[0]); // gets target from first param 
+            if (modelList.Count < 1) return;
+            // if there is no 2nd argument in the param, use "natural"
+            string opt2_string = circles.Length >= 2 ? circles[1] : "natural";
+            bool force = opt2_string != "natural";
+            bool both = opt2_string == "both";
+            bool resistancebreak = circles.Length <= 2;
 
-            		// possible cases based off of previous parameters
-			if (force) targetModel.BreakForcely(modular.modsa_unitModel, abilitySourceType, modular.battleTiming, false, modular.modsa_selfAction);
-			if (!force || both) targetModel.Break(modular.modsa_unitModel, modular.battleTiming, modular.modsa_selfAction);
-			if (resistancebreak) targetModel.ChangeResistOnBreak();
+            // loop over every possible target in the list
+            foreach (BattleUnitModel targetModel in modelList)
+            {
+                // code to change the ability source depending on what the script is run on, a Skill or a Passive
+                ABILITY_SOURCE_TYPE abilitySourceType = ABILITY_SOURCE_TYPE.SKILL;
+                if (modular.abilityMode == 2) abilitySourceType = ABILITY_SOURCE_TYPE.PASSIVE;
+
+                // possible cases based off of previous parameters
+                if (force)
+                    targetModel.BreakForcely(modular.modsa_unitModel, abilitySourceType, modular.battleTiming,
+                        false, modular.modsa_selfAction);
+                if (!force || both)
+                    targetModel.Break(modular.modsa_unitModel, modular.battleTiming, modular.modsa_selfAction);
+                if (resistancebreak) targetModel.ChangeResistOnBreak();
+            }
         }
     }
+
     public override void Load()
     {
-        MainClass.consequenceDict["myConsequence"] = new myConsequenceClass(); // Adds the consequence to the consequence dict in modular
-        LetheHooks.LOG.LogError("THE MODULAR CONSEQUNCE HAS ARRIVED!!!"); // optional logging, feel free to remove.
+        // Adds the consequence to the consequence dict in modular
+        MainClass.consequenceDict["myConsequence"] = new MyConsequenceClass();
+        // optional logging, feel free to remove.
+        LetheHooks.LOG.LogError("THE MODULAR CONSEQUNCE HAS ARRIVED!!!");
     }
-   
 }
-
-
-
